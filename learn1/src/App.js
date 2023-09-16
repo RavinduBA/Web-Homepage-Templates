428fdaeba7d93762c3b9cd898baf6388cd7a1213
RavinduBA <e19004@eng.pdn.ac.lk>
2023-09-16 09:24:39 +0530

    commit1

---
+++ learn1/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+
+
+export default function App(){
+
+
+
+
+   return(  
+   
+
+ )
+
+
+
+}
---
+++ learn1/src/data.js
@@ -0,0 +1,98 @@
+
+
+
+export default [
+
+{
+    id: 1,
+    img:    "./images/swimmer.jpg" ,
+    rating: "5.0",     // if you supposed to do math with no then surround it with curly braces
+    reviewCount: 6,
+    location:  "USA",
+    title:   "Life lessons with Katie Zaferes",
+    prize:    136,
+    openSpots :0
+},
+{   
+    id: 2,
+    img:    "./images/bride.png" ,
+    rating: "5.0",     
+    reviewCount: 30,
+    location:  "Online",
+    title:   "Learn wedding photography",
+    prize:    125,
+    openSpots :1
+},
+
+{   
+    id: 3,
+    img:    "./images/bike.png" ,
+    rating: "4.8",     
+    reviewCount: 2,
+    location:  "Online",
+    title:   "Group Mountain Biking",
+    prize:    50,
+    openSpots :1
+},
+{   
+    id: 4,
+    img:    "./images/bike.png" ,
+    rating: "4.8",     
+    reviewCount: 2,
+    location:  "USA",
+    title:   "Group Mountain Biking",
+    prize:    50,
+    openSpots :1
+},
+{   
+    id: 5, 
+    img:    "./images/bike.png" ,
+    rating: "4.8",     
+    reviewCount: 2,
+    location:  "USA",
+    title:   "Group Mountain Biking",
+    prize:    50, 
+    openSpots :0
+}
+,{
+    id: 6,
+    img:    "./images/bike.png" ,
+    rating: "4.8",     
+    reviewCount: 2,
+    location:  "USA",
+    title:   "Group Mountain Biking",
+    prize:    50,
+    openSpots :1
+},
+{
+    id: 7,
+    img:    "./images/bike.png" ,
+    rating: "4.8",     
+    reviewCount: 2,
+    location:  "USA",
+    title:   "Group Mountain Biking",
+    prize:    50,
+    openSpots :1
+},
+{
+    id: 8,
+    img:    "./images/bike.png" ,
+    rating: "4.8",     
+    reviewCount: 2,
+    location:  "USA",
+    title:   "Group Mountain Biking",
+    prize:    50,
+    openSpots :0
+}
+
+
+
+
+
+
+
+
+
+
+
+]
